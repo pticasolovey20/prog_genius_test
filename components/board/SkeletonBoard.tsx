97da797b406f306bf4b1c42edab4d15e1ad447f6
Dmytro Soloviev <dy.solovyov.work@gmail.com>
2025-06-24 12:52:23 +0300

    updated emotion form

--- components/board/SkeletonBoard.tsx
+++ components/board/SkeletonBoard.tsx
@@ -2,9 +2,9 @@ import { Skeleton } from "@/components/ui/skeleton";
 
 const SkeletonBoard = () => {
   return (
-    <div className="flex flex-col md:grid grid-cols-2 lg:grid-cols-3 gap-4">
+    <div className="w-full flex flex-col md:grid grid-cols-2 lg:grid-cols-3 gap-4">
       {Array.from({ length: 3 }).map((_, index) => (
-        <Skeleton key={index} className="h-[300px] rounded-md" />
+        <Skeleton key={index} className="w-full h-[300px] rounded-md" />
       ))}
     </div>
   );
